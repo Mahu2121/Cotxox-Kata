4c186179466acaef73f3880af5ed8a92e5bf6538
Mahu2121 <hugogbmahu@gmail.com>
2025-03-26 22:37:52 +0100

    feat(Carrera): crear todos los metodos que requiere hasta ahora el main y declarar variables

---
+++ src/main/java/edu/estatuas/Carrera.java
@@ -0,0 +1,82 @@
+package edu.estatuas;
+
+public class Carrera {
+
+    private String tarjetaCredito;
+    private String origen;
+    private String destino;
+    private double distancia;
+    private int tiempoEsperado;
+    private int tiempoCarrera;
+    private double costeTotal;
+    private int propina;
+
+    Carrera(String tarjetaCredito) {
+        this.tarjetaCredito = tarjetaCredito;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public void setTiempoEsperado(int tiempoEsperado) {
+        this.tiempoEsperado = tiempoEsperado;
+    }
+
+    public void setTiempoCarrera(int tiempoCarrera) {
+        this.tiempoCarrera = tiempoCarrera;
+    }
+
+    public void setCosteTotal(double costeTotal) {
+        this.costeTotal = costeTotal;
+    }
+
+    public void setPropina(int propina) {
+        this.propina = propina;
+    }
+
+    public String getTarjetaCredito() {
+        return tarjetaCredito;
+    }
+    public String getOrigen() {
+        return origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public int getTiempoEsperado() {
+        return tiempoEsperado;
+    }
+
+    public int getTiempoCarrera() {
+        return tiempoCarrera;
+    }
+
+    public double getCosteTotal() {
+        return costeTotal;
+    }
+
+    public int getPropina() {
+        return propina;
+    }
+
+    public double getCosteEsperado() {
+        return Tarifa.getCosteTotalEsperado(this);
+    }
+
+
+}
